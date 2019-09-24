90d12378a9a9b17c5202864e2704033d415b8573
王炬 <wangju@bjknrt.com>
2019-09-24 11:59:03 +0800

    新后台-jarvis，让我们欢迎新成员！

---
+++ app/jarvis/controller/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\jarvis\controller;
+
+use app\BaseController;
+
+class User extends BaseController
+{
+    public function index()
+    {
+        halt('aaaaaaa');
+    }
+   
+    
+}
---
+++ app/jarvis/middleware/SessionCookie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\jwin\middleware;
+
+use think\facade\Session;
+use think\facade\Cookie;
+
+/**
+ *	验证是否登录 
+ *
+ */
+class SessionCookie
+{
+    public function handle($request, \Closure $next)
+    {
+
+        if ($request->param('name') == 'session') {
+            return redirect('Index/hello');
+        }
+
+        return $next($request);
+    }
+}
