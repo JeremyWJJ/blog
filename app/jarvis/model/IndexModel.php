9e9e8bd0c094cb1cf2d64cbcd70adcf30bd7ddae
王炬 <wangju@bjknrt.com>
2019-10-11 17:09:02 +0800

    bug

--- app/jarvis/model/IndexModel.php
+++ app/jarvis/model/IndexModel.php
@@ -38,7 +38,7 @@ class IndexModel extends Model
         $last = Db::table('log')
         ->leftJoin('admin','admin.id = log.user_id')
         ->where('admin.username',$admin->name)
-        ->order('ctime','desc')
+        ->order('log.ctime','desc')
         ->find();
 
     	return array(
